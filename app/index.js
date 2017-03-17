57094c1472407b451eb34c28f80c0f8b9ff6d750
vikholdon <1073848599@qq.com>
2017-03-17 11:11:37 +0800

    修改

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 require('../main.scss');
 require('../main.css');
-var suhbb = require('./sub');
+var sub = require('./sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World- --  --</h1>';
 app.appendChild(sub());
