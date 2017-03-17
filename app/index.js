420b65c30749512f9477a9f1c5c884982dcc4542
vikholdon <1073848599@qq.com>
2017-03-17 11:13:48 +0800

    增加

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ require('../main.scss');
 require('../main.css');
 var sub = require('./sub');
 var app  = document.createElement('div');
-app.innerHTML = '<h1>Hello World- --  --</h1>';
+app.innerHTML = '<h1>Hello World- -我再次修噶-  --</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
